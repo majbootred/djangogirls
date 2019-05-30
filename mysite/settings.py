faef102d22d13f00234969d412c9546e365b3c49
Marlen Jacob <maj@riseup.net>
2019-05-30 11:51:31 +0200

    add jquery call to bootstrap js call

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'j_^ub^aj8gberlaq91swr8kb7%20*6x)11w8k1-qvn-7bn9((x'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'majbootred.pythonanywhere.com']
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -94,9 +91,6 @@ DATABASES = {
     }
 }'''
 
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -115,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -129,22 +122,21 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#for app specific files
+# for app specific files
 STATIC_URL = '/static/'
 
-#for generic files
+# for generic files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets")
 ]
 
-#for productive use this
+# for productive use this
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#collect all static files in this folder
+# collect all static files in this folder
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
