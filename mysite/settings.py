a7de6210ef240e0bfee4e4fda26cc2e7524f0223
Marlen Jacob <maj@riseup.net>
2019-05-30 11:24:06 +0200

    some explanation for the staic serving

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,13 +133,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#for app specific files
 STATIC_URL = '/static/'
+
+#for generic files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets")
 ]
+
+#for productive use this
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+#collect all static files in this folder
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
